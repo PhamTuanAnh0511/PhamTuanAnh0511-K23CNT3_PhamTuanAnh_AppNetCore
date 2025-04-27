2e51a082281bb14f7529380af0abbf4215827a41
unknown <sktkgame69@gmail.com>
2025-04-27 16:41:04 +0700

    Lab01

---
+++ Myapp1/Controllers/Demo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Myapp1.Controllers
+{
+    public class Demo : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Myapp1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Myapp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
